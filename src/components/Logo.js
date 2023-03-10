bbb6a5170adc32a88cb1e61fb0cc78bf859c857e
tarunsoni96 <tarsoni69@gmail.com>
2023-03-11 01:33:55 +0530

    latest

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+function Logo(props) {
+    return (
+        <div {...props}>
+            <span className="wideFont sectionTitle" style={{  }}>
+                PXDP
+            </span>
+        </div>
+    )
+}
+
+export default Logo
