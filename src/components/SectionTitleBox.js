c4cad0f6721cec4acc7b08cf5d882a363e6a6609
tarun soni <tarunsoni@taruns-MacBook-Air.local>
2023-03-10 22:30:42 +0530

    in prog

--- src/components/SectionTitleBox.js
+++ src/components/SectionTitleBox.js
@@ -3,25 +3,43 @@ import { commonBorderStyle } from '../styles/CommonStyles'
 function SectionTitleBox(props) {
     return (
         <div
+        className='container-fluid'
+
             style={{
                 paddingTop: 70,
                 marginBottom: props.marginBottom || 0,
                 display: 'flex',
                 flexDirection: 'column',
                 borderBottom: props.hideBorder ? 0 : commonBorderStyle,
-                paddingBottom:props.hideBorder ? 0 : 15
+                paddingBottom: props.hideBorder ? 0 : 15
             }}
         >
-            <span className="wideFont sectionTitle caps" style={{}}>{props.sectionTitle}</span>
-            <div style={{ flexDirection: 'row',alignItems:'flex-start', marginTop: 0, justifyContent: 'space-between',display: props.description ? 'flex' : 'block' }}>
-                <span className="wideFont caps" style={{ fontSize: 90, fontWeight: 900 }}>
-                    {props.title}
-                </span>
-                {props.description && (
-                    <span className="textDescriptionBold lightColorDescription " style={{marginTop:16, fontSize: 18, maxWidth: 300 }}>
-                        {props.description}
+            <div className="row align-items-end" >
+                <div className="col-12 col-sm-12 col-md-9" style={{}}>
+                    <span className="wideFont sectionTitle caps" style={{}}>
+                        {props.sectionTitle}
                     </span>
-                )}
+                    <div
+                        style={{
+                            flexDirection: 'row',
+                            alignItems: 'flex-start',
+                            marginTop: 0,
+                            justifyContent: 'space-between',
+                            display: props.description ? 'flex' : 'block'
+                        }}
+                    >
+                        <span className="wideFont caps bold sectionMainTitle" style={{  }}>
+                            {props.title}
+                        </span>
+                    </div>
+                </div>
+                <div className="col-12 col-sm-12 col-md-12 col-lg-3" style={{}}>
+                    {props.description && (
+                        <span className="textDescriptionBold lightColorDescription " style={{ fontSize: 18, }}>
+                            {props.description}
+                        </span>
+                    )}
+                </div>
             </div>
         </div>
     )
